df1a9c6b71b899f1f1cb7238b36e934a698e0eb2
Ravshan Samandarov <ravshansbox@gmail.com>
2025-02-20 06:49:17 +0300

    handle dom events

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,13 @@ import { createElement, createRoot } from '@/lib';
 const element = (
   <h1 id="message">
     Hello, <i>World</i>
+    <button
+      onClick={(event) => {
+        console.log(event, 'Greetings!');
+      }}
+    >
+      Greet
+    </button>
   </h1>
 );
 
