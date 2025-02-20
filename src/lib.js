994c126d3c624c17e062431c551e5e32c17f2914
Ravshan Samandarov <ravshansbox@gmail.com>
2025-02-20 05:31:27 +0300

    implement `createTextNode`

--- src/lib.js
+++ src/lib.js
@@ -1,3 +1,15 @@
 export const createElement = (type, props, ...children) => {
-  return { type, props: { ...props, children } };
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map((child) =>
+        typeof child === 'object' ? child : createTextNode(child),
+      ),
+    },
+  };
+};
+
+export const createTextNode = (nodeValue) => {
+  return { type: 'TEXT_NODE', props: { nodeValue, children: [] } };
 };
