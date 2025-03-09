8566499319fdbafbf59f175ee0e99899b9fa055d
Ravshan Samandarov <ravshansbox@gmail.com>
2025-03-09 23:10:24 +0300

    implement rendering an element vdom node

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,31 @@
+const createElement = (type, props, ...children) => {
+  return { type, props: { ...props, children } };
+};
+
+const createTextNode = (text) => {
+  return document.createTextNode(text);
+};
+
+const createElementNode = ({ type, props }) => {
+  const domElement = document.createElement(type);
+  for (const child of props.children) {
+    render(domElement, child);
+  }
+  return domElement;
+};
+
+const render = (domElement, vdomNode) => {
+  domElement.appendChild(
+    ['string', 'number', 'boolean'].includes(typeof vdomNode)
+      ? createTextNode(vdomNode)
+      : createElementNode(vdomNode),
+  );
+};
+
 const createRoot = (domElement) => {
   return {
-    render: (vdomNode) => {
-      const textNode = document.createTextNode(vdomNode);
-      domElement.appendChild(textNode);
-    },
+    render: render.bind(null, domElement),
   };
 };
 
-createRoot(document.getElementById('root')).render('Hello');
+createRoot(document.getElementById('root')).render(<h1 id="message">Hello</h1>);
