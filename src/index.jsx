4e2ff93d0076f8b5e932fc590329d7d887528163
Ravshan Samandarov <ravshansbox@gmail.com>
2025-03-10 15:42:39 +0300

    implement `render`

--- src/index.jsx
+++ src/index.jsx
@@ -1,3 +1,29 @@
 function createElement(type, props, ...children) {
   return { type, props: { ...props, children } };
 }
+
+function isTextNode(vdomNode) {
+  return ['string', 'number', 'boolean'].includes(typeof vdomNode);
+}
+
+function render(domElement, vdomNode) {
+  let domNode;
+  if (isTextNode(vdomNode)) {
+    domNode = document.createTextNode(data);
+  } else {
+    domNode = document.createElement(vdomNode.type);
+    for (const child of vdomNode.props.children) {
+      render(domNode, child);
+    }
+    return domNode;
+  }
+  domElement.appendChild(domNode);
+}
+
+function createRoot(domElement) {
+  return {
+    render(vdomNode) {
+      render(domElement, vdomNode);
+    },
+  };
+}
